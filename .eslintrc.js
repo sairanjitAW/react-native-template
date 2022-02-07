d31b06f5179b6616daff6b925cb95bd409d65c72
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2022-02-07 19:34:16 +0530

    chore: eslint setup with typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
 };
